bee54f82db1590f84ff206282b70cfa98ac9df3d
mate kopaliani <114822928+Mate-Kopaliani@users.noreply.github.com>
2023-07-11 11:46:01 +0400

    Update Main.java

--- src/main/java/com/algorithms/Main.java
+++ src/main/java/com/algorithms/Main.java
@@ -1,13 +1,1 @@
-package com.algorithms;
 
-public class Main {
-    public static void main(String[] args) {
-        BST<Integer> bst = new BST<>(0);
-        bst.insert(2);
-        bst.insert(-1);
-        bst.insert(3);
-        System.out.println(bst.toList());
-        bst.invert();
-        System.out.println(bst.toList());
-    }
-}
